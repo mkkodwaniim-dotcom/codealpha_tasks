1fb1d643db3b984c026d63a0348f94c6cb545a10
mkkodwaniim-dotcom <mkkodwaniim@gmail.com>
2026-05-31 18:29:28 -0400

    Add files via upload

---
+++ basic chatbox.py
@@ -0,0 +1,20 @@
+def chatbot():
+    print("Simple Chatbot Started!")
+    
+    while True:
+        user = input("You: ").lower()
+
+        if user == "hello":
+            print("Bot: Hi!")
+        
+        elif user == "how are you":
+            print("Bot: I'm fine, thanks!")
+        
+        elif user == "bye":
+            print("Bot: Goodbye!")
+            break
+        
+        else:
+            print("Bot: Sorry, I don't understand.")
+
+chatbot()
